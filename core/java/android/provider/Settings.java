ca3b9c34084d52e134c10d462bef503c75d8d65d
beanstown106 <nbenis106@gmail.com>
2019-10-15 09:43:58 +0200

    Custom Carrier Label & Carrier Label Placement [1/2]
    Squash Of:
    Keyguard/Statusbar Carrier label options [1/2]
    * Stock behavior(only on keyguard), only on statusbar,both disabled or both enabled
    * code originally writted by @Altaf-Mahdi i just squased into one commit
    PureNexusProject-Legacy/android_frameworks_base@688d56f
    
    Show carrier label / custom & change color [1/2]
    *code originally written by @martincz
    *includes various fixs by the du team
    PureNexusProject-Legacy/android_frameworks_base@16a247a
    
    Carrier label - fix capitilization bug
    PureNexusProject-Legacy/android_frameworks_base@4b9e22b
    
    remove carrier label color and make carrier label play nice with dark statusbar
    PureNexusProject-Legacy/android_frameworks_base@3f32102
    
    Fix Carrier Label font size not changing on system font size change
    DirtyUnicorns/android_frameworks_base@8da9dcf
    
    include carrier label in icon merger width calculation
    PureNexusProject-Legacy/android_frameworks_base@9df3a0c
    
    Credit/Thanks to @Altaf-Mahdi @beanstown106 @dwitherell and @martincz
    
    BenzoEdit: Fixed statusbar layout for pie
    
    [spezi77] Updated for 12.1.0_r1
    - Added the call to register a ContentObserver to KeyguardStatusBarViewController
      due to latest AOSP code changes
    
    Change-Id: I81157dd09d058b70a3e19a8242c81765bc6d6282
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6785,6 +6785,23 @@ public final class Settings {
         public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
 
         /**
+         * Status bar carrier label
+         * 0: Hide
+         * 1: Display on keyguard status bar
+         * 2: Display on Normal status bar
+         * 3: Enabled for both
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_CARRIER = "status_bar_show_carrier";
+
+        /**
+         * custom carrier label. The value is
+         * String.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
