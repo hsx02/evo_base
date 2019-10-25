69bff3f5b431f0908c3ddce3ab1bd1fdf8d56b29
El Dainosor <eldainosor@gmail.com>
2019-10-25 07:16:45 +0000

    settings: Protect sensitive info on About device
    Tired of seeing people/see yourself editing every screenshot to hide some personal info like their phone numbers or their IMEI? Well, this commit will help by adding a custom string to inform the user that, to get that info, they should tap that preference. If you want to show them, you can make an overlay and set configShowDeviceSensitiveInfo as true.
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ibbb68bafe51634c92d009909a38d47bcdd8fdf04

--- src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
@@ -64,6 +64,7 @@ public class PhoneNumberPreferenceController extends BasePreferenceController
     private HashMap<Integer, ImsConnector> mImsConnectorMap = new HashMap<>();
     private int mPhoneCount;
     private Handler mHandler;
+    private boolean mTapped = false;
 
     private final ImsMmTelManager.RegistrationCallback mImsRegistrationCallback =
             new ImsMmTelManager.RegistrationCallback() {
@@ -94,7 +95,10 @@ public class PhoneNumberPreferenceController extends BasePreferenceController
 
     @Override
     public CharSequence getSummary() {
-        return getFirstPhoneNumber();
+        if (mContext.getResources().getBoolean(R.bool.configShowDeviceSensitiveInfo) && mTapped) {
+            return getFirstPhoneNumber();
+        }
+        return mContext.getString(R.string.device_info_protected_single_press);
     }
 
     @Override
@@ -127,7 +131,29 @@ public class PhoneNumberPreferenceController extends BasePreferenceController
     }
 
     @Override
+    public boolean isSliceable() {
+        return mTapped;
+    }
+
+    @Override
+    public boolean isCopyableSlice() {
+        return mTapped;
+    }
+
+    @Override
     public boolean useDynamicSliceSummary() {
+        return mTapped;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        final int simSlotNumber = mPreferenceList.indexOf(preference);
+        if (simSlotNumber == -1) {
+            return false;
+        }
+        mTapped = true;
+        final Preference simStatusPreference = mPreferenceList.get(simSlotNumber);
+        simStatusPreference.setSummary(getPhoneNumber(simSlotNumber));
         return true;
     }
 
@@ -159,7 +185,11 @@ public class PhoneNumberPreferenceController extends BasePreferenceController
             return mContext.getText(R.string.device_info_default);
         }
 
-        return getFormattedPhoneNumber(subscriptionInfo);
+        if (mContext.getResources().getBoolean(R.bool.configShowDeviceSensitiveInfo) || mTapped) {
+            return getFormattedPhoneNumber(subscriptionInfo);
+        }
+        return mContext.getString(R.string.device_info_protected_single_press);
+
     }
 
     private CharSequence getPreferenceTitle(int simSlot) {
