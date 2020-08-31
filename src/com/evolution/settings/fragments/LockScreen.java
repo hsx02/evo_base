bc3ed2eec7b4569e7f1416343c7f41fb84fd9d86
Varun Date <date.varun123@gmail.com>
2020-09-01 04:58:58 +1000

    Evolver: Disable Force FP authentication preference if encrypted
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- src/com/evolution/settings/fragments/LockScreen.java
+++ src/com/evolution/settings/fragments/LockScreen.java
@@ -28,6 +28,7 @@ import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.os.UserHandle;
 import android.provider.Settings;
+import com.android.internal.widget.LockPatternUtils;
 
 import androidx.preference.SwitchPreference;
 import androidx.preference.ListPreference;
@@ -43,6 +44,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.evolution.settings.preference.SystemSettingSwitchPreference;
 import com.evolution.settings.preference.SystemSettingListPreference;
 
 import java.util.ArrayList;
@@ -55,10 +57,12 @@ public class LockScreen extends SettingsPreferenceFragment implements
     private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
     private static final String FINGERPRINT_CATEGORY = "lockscreen_fingerprint_category";
     private static final String UDFPS_CATEGORY = "udfps_category";
+    private static final String FP_KEYSTORE = "fp_unlock_keystore";
 
     private FingerprintManager mFingerprintManager;
     private PreferenceCategory mFingerprintCategory;
     private PreferenceCategory mUdfpsCategory;
+    private SystemSettingSwitchPreference mFingerprintUnlock;
 
     static final int MODE_DISABLED = 0;
     static final int MODE_NIGHT = 1;
@@ -93,6 +97,18 @@ public class LockScreen extends SettingsPreferenceFragment implements
 
         mAODPref = findPreference(AOD_SCHEDULE_KEY);
         updateAlwaysOnSummary();
+
+        mFingerprintUnlock = (SystemSettingSwitchPreference) findPreference(FP_KEYSTORE);
+
+        if (mFingerprintUnlock != null) {
+           if (LockPatternUtils.isDeviceEncryptionEnabled()) {
+               mFingerprintUnlock.setEnabled(false);
+               mFingerprintUnlock.setSummary(R.string.fp_encrypt_warning);
+            } else {
+               mFingerprintUnlock.setEnabled(true);
+               mFingerprintUnlock.setSummary(R.string.fp_unlock_keystore_summary);
+            }
+        }
     }
 
     @Override
