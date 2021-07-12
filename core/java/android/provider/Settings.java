9dc2b3d5fc5390579ce005137d006646bbe8cb81
Alex Cruz <du.alexcruz@gmail.com>
2021-07-12 19:25:49 +0530

    Lockscreen timeout: Make timeout configurable [1/2]
    SDB - adapt for A12 changes
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6779,6 +6779,13 @@ public final class Settings {
          */
 
         /**
+         * The amount of time in milliseconds before the device goes to sleep or begins
+         * to dream after a period of inactivity in the keyguard.
+         * @hide
+         */
+        public static final String LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
