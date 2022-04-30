05a03e9b178be722811e6332b424b35e5d80e6b4
spkal01 <kalligeross@gmail.com>
2022-04-30 15:28:46 +0300

    SystemUI: Transparent QS customization
    Inspired by dotOS
    
    @neobuddy89: Clean up unused code and convert to 1-100 scale.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6786,6 +6786,11 @@ public final class Settings {
         public static final String LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
 
         /**
+         * @hide
+         */
+        public static final String QS_TRANSPARENCY = "qs_transparency";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
