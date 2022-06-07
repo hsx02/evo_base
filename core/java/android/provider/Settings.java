f2693cfa19cef725c65b3e32bcdd4db9967a2314
StarkDroid <gamerprince.exp@gmail.com>
2022-06-07 16:17:49 -0500

    [1/2] Clock: Add ability to resize QS & Statusbar Clock
    We had this in A11, So now forward it to A12
    
    spkal01: Utilize Tuner Settings and simplify implementation
    
    Co-Authored-By: spkal01 <kalligeross@gmail.com>
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6677,6 +6677,20 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+
+        /**
+         * Clock font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SIZE = "status_bar_clock_size";
+
+        /**
+         * Clock font size QS
+         * @hide
+         */
+        public static final String QS_HEADER_CLOCK_SIZE = "qs_header_clock_size";
+
+        /**
          * Change quick settings tiles animation style
          * @hide
          */
