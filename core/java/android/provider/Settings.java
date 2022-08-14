8d37e72cc2b88bf41dbb4aa0831cbca0cf63e4f8
Sonal Singh <sonal.singh.19993@gmail.com>
2022-08-14 14:47:54 +0530

    Partial Revert "[SQUASH] Combine multiple reverts"
    This reverts commit e124a7b66ada47dafbad0c47ce4aedfd1a80307e.
    
    Revert "MediaPlayer: Shift background shade to system_accent1_400"
    
    This reverts commit b32f12a21525e9cc12a8517bc0f3ecaabb652a66.
    
    Revert "Fix wakelock under-locked issue"
    
    This reverts commit 7c0b434a9f7b6b7bb30f153d71a616b566228d5a.
    
    Revert "Skip checking SystemUI's permission for observing sensor privacy"
    
    This reverts commit 09d914105b92d06cbfe33f17c5a5ba6feefbd3bf.
    
    Revert "udfps: Implement framework dimming support"
    
    This reverts commit 4016d4acb48a10b69b63cfa8f848989343ed7323.
    
    Revert "fonts: Use variable font for Roboto Regular"
    
    This reverts commit 7b63d28647d42bad4d04f0eeb276cdd28ea91796.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12213,6 +12213,12 @@ public final class Settings {
                 "device_state_rotation_lock";
 
         /**
+         * Whether to show Wi-Fi standard icon
+         * @hide
+         */
+        public static final String SHOW_WIFI_STANDARD_ICON = "show_wifi_standard_icon";
+
+        /**
          * Gesture navbar length mode.
          * Supported modes: 0 for normal length, 1 for medium and 2 for long.
          * Default 0.
@@ -12311,6 +12317,13 @@ public final class Settings {
          * @hide
          */
         public static final String SWAP_CAPACITIVE_KEYS = "swap_capacitive_keys";
+
+        /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
          * Whether to show the brightness slider in quick settings panel.
@@ -18435,6 +18448,12 @@ public final class Settings {
          * @hide
          */
         public static final String RESTRICTED_NETWORKING_MODE = "restricted_networking_mode";
+
+        /**
+         * Control whether application downgrade is allowed.
+         * @hide
+         */
+        public static final String PM_DOWNGRADE_ALLOWED = "pm_downgrade_allowed";
 
         /**
          * Enable blocking wakelock
